a8e9e33cd87f4ef17094d2f2a1b5f6a8d58ce3b9
Filip Kobiec <fipis14@gmail.com>
2020-09-15 14:44:51 +0200

    Implement Square and OceanDisplayer classes

--- Stateczki/Game.cs
+++ Stateczki/Game.cs
@@ -6,7 +6,15 @@ namespace Stateczki
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Square[,] exampleOcean = new Square[3,3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    exampleOcean[i, j] = new Square();
+                }
+            }
+            OceanDisplayer.PrintOceanForPlay(exampleOcean);
         }
     }
 }
--- Stateczki/Oceans/OceanDisplayer.cs
+++ Stateczki/Oceans/OceanDisplayer.cs
@@ -6,5 +6,69 @@ namespace Stateczki
 {
     static class OceanDisplayer
     {
+        private static Dictionary<string, string> symbols = new Dictionary<string, string>
+        {
+            {"ship", "S" },
+            {"shoot", "X" },
+            {"empty", "O" }
+        };
+
+        public static void PrintOceanForPlay(Square[,] ocean)
+        {
+            var rows = ocean.GetLength(0);
+            var cols = ocean.GetLength(1);
+
+            char[] alphabet = InitAlphabet(rows);
+            PrintHeaders(cols);
+            PrintGameRows(ocean, rows, cols, alphabet);
+        }
+
+        private static void PrintGameRows(Square[,] ocean, int rows, int cols, char[] alphabet)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("\n" + alphabet[i] + " | ");
+                for (int j = 0; j < cols; j++)
+                {
+                    var currentSquare = ocean[i, j];
+                    if (currentSquare.IsOccupied)
+                    {
+                        Console.Write(symbols["ship"] + " | ");
+                    }
+                    else
+                    {
+                        Console.Write(symbols["empty"] + " | ");
+                    }
+                }
+            }
+        }
+
+        private static void PrintHeaders(int cols)
+        {
+            Console.Write("# | ");
+            for (int counterRowsNumbers = 1; counterRowsNumbers <= cols; counterRowsNumbers++)
+            {
+                if (counterRowsNumbers < 9)
+                {
+                    Console.Write(counterRowsNumbers + " | ");
+                }
+                else
+                {
+                    Console.Write(counterRowsNumbers + " |");
+                }
+            }
+        }
+
+        private static char[] InitAlphabet(int rows)
+        {
+            var alphabet = new char[rows];
+
+            for (var i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = (char)(65 + i);
+            }
+
+            return alphabet;
+        }
     }
 }
