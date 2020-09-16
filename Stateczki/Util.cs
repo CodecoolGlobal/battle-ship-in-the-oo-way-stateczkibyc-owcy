4f4c50fb1f8ada235bc1b4f64484f76c8cf3a6cf
Filip Kobiec <fipis14@gmail.com>
2020-09-16 20:03:55 +0200

    Add util class

---
+++ Stateczki/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stateczki
+{
+    public static class Util
+    {
+        public static string ToSymbolForOpponent(this SquareStatus status) => status switch
+        {
+            SquareStatus.Empty => "O | ",
+            SquareStatus.HitShip => "X | ",
+            SquareStatus.Ship => "O | ",
+            SquareStatus.SunkShip => "S | ",
+            SquareStatus.Miss => "M | ",
+            _ => throw new ArgumentOutOfRangeException(),
+        };
+
+        public static string ToSymbolForCurrentPlayer(this SquareStatus status) => status switch
+        {
+            SquareStatus.Empty => "O | ",
+            SquareStatus.HitShip => "X | ",
+            SquareStatus.Ship => "V | ",
+            SquareStatus.SunkShip => "S | ",
+            SquareStatus.Miss => "M | ",
+            _ => throw new ArgumentOutOfRangeException(),
+        };
+    }
+}
